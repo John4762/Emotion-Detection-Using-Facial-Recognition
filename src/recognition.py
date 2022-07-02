e804b0a617b383ab148941a92ffaa8d46d7c54bc
joannejuneie <joannekather12@gmail.com>
2022-07-03 01:48:31 +0530

    linking

--- src/recognition.py
+++ src/recognition.py
@@ -5,8 +5,8 @@ import os
 from file_selector import file_select 
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('C:/Users/hp/OneDrive/Documents/GitHub/S6 MiniProject/Emotion-Detection-Using-Facial-Recognition/src/trainer/trainer.yml')
-cascadePath = "C:/Users/hp/OneDrive/Documents/GitHub/S6 MiniProject/Emotion-Detection-Using-Facial-Recognition/src/haarcascade_frontalface_default.xml"
+recognizer.read('C:/Users/Dell/Desktop/Emotion-Detection-Using-Facial-Recognition/src/trainer/trainer.yml')
+cascadePath = "C:/Users/Dell/Desktop/Emotion-Detection-Using-Facial-Recognition/src/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -55,8 +55,10 @@ for(x,y,w,h) in faces:
     
     cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
    # cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-
-cv2.imwrite("C:/Users/hp/OneDrive/Documents/GitHub/S6 MiniProject/Emotion-Detection-Using-Facial-Recognition/src/generated_pic.jpg",img) 
+f = open("result.txt", "w")
+f.write(str(id))
+f.close()
+cv2.imwrite("C:/Users/Dell/Desktop/Emotion-Detection-Using-Facial-Recognition/src/generated_pic.jpg",img) 
 
 print("\n [INFO] Done detecting and Image is saved")
 cam.release()
