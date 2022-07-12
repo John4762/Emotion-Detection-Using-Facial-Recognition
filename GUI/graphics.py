8c9e564da528b38c5c0e651e2b28dbac817599c4
Jithin010 <jithinmathew1208@gmail.com>
2022-07-12 10:08:19 +0530

    Gui Success

--- GUI/graphics.py
+++ GUI/graphics.py
@@ -9,15 +9,9 @@ import os
 Builder.load_file('graphics.kv')
 
 class ScreenOne(Screen):
-	def callback(self,instance):
-		os.system('python E:/BTech/S6/MP/Music_Recomendation_System/Emotion-Detection-Using-Facial-Recognition/src/recognition.py')
-	
-	def __init__(self,**kwargs):
-		super(ScreenOne, self).__init__(**kwargs)
-		mybutton = Button(text = "Submit")
-		mybutton.bind(on_release = self.callback) 
-		self.add_widget(mybutton)
 
+	def callback(self):
+		os.system('python E:/BTech/S6/MP/Music_Recomendation_System/Emotion-Detection-Using-Facial-Recognition/src/recognition.py')
 	pass
 
 class ScreenTwo(Screen):
--- GUI/tempCodeRunnerFile.py
+++ GUI/tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-self.btnfunc
+	super(ScreenOne, self).__init__(**kwargs)
+	# 	mybutton.bind(on_press = self.callback) 
+	# 	self.add_widget(mybutton)
+
+
+	# def callback(self,instance):
+	# 	print("hello")
