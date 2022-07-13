689b67b1f146632da0160a870be216a65b408e8f
joshna <josh.salu7@gmail.com>
2022-07-13 16:26:14 +0530

    modified:   GUI/graphics.kv 	modified:   GUI/graphics.py 	new file:   GUI/logo.png.png

--- GUI/graphics.py
+++ GUI/graphics.py
@@ -2,7 +2,9 @@ import kivy
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 import os
 
@@ -27,7 +29,7 @@ class ScreenFive(Screen):
 	pass
 
 screen_manager = ScreenManager()
-screen_manager.add_widget(ScreenOne(name ="screen_one"))
+screen_manager.add_widget(ScreenOne(name ="screen_one"), Image(source ='logo.png.png')) 
 screen_manager.add_widget(ScreenTwo(name ="screen_two"))
 screen_manager.add_widget(ScreenThree(name ="screen_three"))
 screen_manager.add_widget(ScreenFour(name ="screen_four"))
